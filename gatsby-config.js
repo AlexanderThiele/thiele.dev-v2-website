09417d83b07f2b6b4f98fdb871915d5390de591f
alexanderthiele <alex@thiele.dev>
2022-06-30 08:33:36 +0200

    sitemeta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Developer Blog by Alexander Thiele",
+    description: "Doing Flutter, Kotlin, Gcloud, Ktor, Java",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
